b34d34622384ad1d281a5649d11a499f10b34c62
Jonathan Kerr <jonathandrewkerr@gmail.com>
2020-02-02 12:16:52 +0000

    Use the app factory pattern
    This is a significant change. To enable testing, I've refactored the application to use an 'app factory' pattern. What this means is that the application is now created by calling the 'create_app' method in app/__init__. This in turn allows us to create the app with different configurations for different scenarios - in the simplest case, for running in a development mode versus a test mode.
    
    Using this pattern opens the door to using blueprints, which allow us to group routes together in logically similar patterns. Advanced use of blueprints allows us to add paths to every route in the blueprint and thereby separate them - for example, allowing for '/user/create' and '/masterclass/create'. Both are served by a route called "create", but because they're under different blueprints, there's no clash. I can walk you through an example when I see you next.

--- app/__init__.py
+++ app/__init__.py
@@ -4,13 +4,22 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 
-app = Flask(__name__)
-app.config.from_object(Config)
-db = SQLAlchemy(app)
-migrate = Migrate(app, db)
-login = LoginManager(app)
-login.login_view = 'login'
+db = SQLAlchemy()
+migrate = Migrate()
+login = LoginManager()
+login.login_view = 'main_bp.login'
 
 
+def create_app(config_class=Config):
+    app = Flask(__name__)
+    app.config.from_object(config_class)
 
-from app import routes, models
+    db.init_app(app)
+    migrate.init_app(app, db)
+    login.init_app(app)
+
+    from app.routes import main_bp
+
+    app.register_blueprint(main_bp)
+
+    return app
